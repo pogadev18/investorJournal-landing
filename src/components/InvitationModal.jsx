7e96fa72b05f7071d935285e260f61a5e4f00edf
pogadev <pogadev18@gmail.com>
2023-02-08 10:20:39 +0200

    newsletter fixed on mobile

--- src/components/InvitationModal.jsx
+++ src/components/InvitationModal.jsx
@@ -68,13 +68,13 @@ export const InvitationModal = ({ setIsOpen }) => {
             className="w-full h-screen sm:h-auto sm:w-3/4 md:w-3/5 lg:w-[1000px] xl:w-[1100px] sm:rounded-2xl bg-customDarkBgTransparentLighter custom-border-gray-darker py-12 px-8 sm:px-16 backdrop-blur-xl fixed sm:mb-8 fixed mx-auto z-50"
             onClick={(e) => e.stopPropagation()}
           >
-            <div className="flex relative">
-              <div className="w-1/2 hidden lg:inline">
+            <div className="flex flex-col relative md:flex-row">
+              <div className="md:w-1/2 w-full lg:inline">
                 <h2 className="mt-6 mb-2 text-5xl font-bold tracking-normal text-white">
                   Subscribe Now
                 </h2>
 
-                <ul className="mb-6 text-white mt-12">
+                <ul className="mb-2 md:mb-6 text-white mt-12">
                   <li className="mb-4 flex">
                     <CheckArrowIcon />
                     <span>be the first to know about any updates</span>
@@ -89,7 +89,7 @@ export const InvitationModal = ({ setIsOpen }) => {
                   </li>
                 </ul>
               </div>
-              <div className="w-full lg:w-1/2 flex items-center flex-col justify-center pt-24 sm:pt-0">
+              <div className="w-full lg:w-1/2 flex items-center flex-col justify-center pt-8 md:pt-24 sm:pt-0">
                 <div className="flex inline lg:hidden justify-start items-center grow basis-0 mb-8 pr-6">
                   <div className="text-white mr-2 text-8xl">
                     <TailcastLogo />
