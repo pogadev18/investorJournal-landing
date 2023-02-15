4c47fc23975c6c539e07fdc8c036479cc27fcc84
pogadev <pogadev18@gmail.com>
2023-02-15 09:38:32 +0200

    added new images

--- src/components/Features2.jsx
+++ src/components/Features2.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 
-import feature5 from "../assets/images/feature5.jpg";
-import feature6 from "../assets/images/feature6.jpg";
+import feature5 from "../assets/images/feature5.svg";
+import feature6 from "../assets/images/feature6.svg";
 import { CheckArrowIcon } from "../assets/icons/CheckArrowIcon";
 
 export const Features2 = () => (
@@ -16,17 +16,13 @@ export const Features2 = () => (
         <div className="w-11/12 sm:w-3/4 mx-auto lg:w-1/2 flex flex-wrap lg:-mx-4 sm:pr-8 justify-center order-last lg:order-first">
           <div className="mb-8 lg:mb-0 w-full px-2 lg:pl-16 flex flex-col justify-center md:pl-8">
             <div className="mb-4 py-3 md:pl-3 md:pr-20 lg:pr-12 rounded">
-              <img
-                src={feature5}
-                alt="f1"
-                className="rounded-xl  custom-border-gray"
-              />
+              <img src={feature5} alt="f1" className="rounded-xl  " />
             </div>
             <div className="py-3 md:pl-20 lg:pl-12 md:pr-2 rounded ">
               <img
                 src={feature6}
-                alt="f2"
-                className="rounded-xl  custom-border-gray"
+                alt="Investment with Notes"
+                className="rounded-xl"
               />
             </div>
           </div>
